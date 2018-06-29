715da5433edbbf81803c3b8cca651a2f0afb43a6
Oleksandr Bratashov <abratashov@gmail.com>
2018-06-29 14:25:36 +0300

    Fix CORS access for UI

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000', '127.0.0.1:3000', ENV['domain_name'] || Rails.application.secrets[:application][:default_url_options][:host]
-
+    origins 'localhost:3000', 'localhost:8080', '127.0.0.1:3000', '127.0.0.1:8080', ENV['domain_name'] || Rails.application.secrets[:application][:default_url_options][:host]
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: ['access-token', 'client', 'uid']
   end
 end
